3d2a8bc6e22408e7f9931b29abd70e35eebb2265
wolfgang <chw@praqma.net>
2010-12-17 16:09:01 +0100

    Renamed log to report

--- src/main/java/net/praqma/vans/VANSReport.java
+++ src/main/java/net/praqma/vans/VANSReport.java
@@ -1,50 +1,56 @@
 package net.praqma.vans;
 
-import java.io.File;
-import java.util.List;
 
-import javax.xml.transform.stream.StreamSource;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import net.praqma.util.XML;
 import net.praqma.vans.command.Command;
 import net.praqma.vans.filter.Filter.Findings;
 import net.praqma.vans.filter.Finding;
+import net.praqma.vans.util.Status;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public class VANSLog extends XML
+public class VANSReport extends XML
 {
 	
-	public VANSLog( String name )
+	public VANSReport( String name )
 	{
 		super( "cases" );
 		
-		super.getRoot().setAttribute( "name", name );
+		Calendar date        = Calendar.getInstance();
+		SimpleDateFormat timeformat = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" );
+		String timestamp = timeformat.format( date.getTime() );
 		
-		System.out.println( "Creating VANS log" );
+		super.getRoot().setAttribute( "name", name );
+		getRoot().setAttribute( "timestamp", timestamp );
 		
 	}
 	
-	public void addCase( Findings findings, Command cmd )
+	public void addCase( Findings findings, Command cmd, Status status )
 	{
 		Element testcase = addElement( "testcase" );
-		//testcase.setAttribute( "name", name );
+		
+		testcase.setAttribute( "number", findings.size() + "" );
+		testcase.setAttribute( "errors", findings.numberOfErrors() + "" );
 		
 		addElement( testcase, "cwd" ).setTextContent( cmd.getCwd().getAbsolutePath().toString() );
 		addElement( testcase, "command" ).setTextContent( cmd.getCmd() );
 		
-		//info.setAttribute( "cwd", cmd.getCwd().getAbsolutePath().toString() );
-		//info.setAttribute( "cmd", cmd.getCmd() );
-		
 		Element find = addElement( testcase, "findings" );
-		find.setAttribute( "number", findings.size() + "" );
-		find.setAttribute( "errors", findings.numberOfErrors() + "" );
-
-		
+	
 		for( Finding f : findings )
 		{
 			addFinding( f, find );
+		}
+		
+		if( status.failed )
+		{
+			Element failure = addElement( testcase, "failure" );
+			failure.setTextContent( status.text );
+			failure.setAttribute( "message", status.message );
+			failure.setAttribute( "type", status.type );
 		}
 	}
 	
