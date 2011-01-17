76edba3fb72ed3c3f1b2a415424072e5a80564b0
wolfgang <chw@praqma.net>
2011-01-17 23:06:23 +0100

    Added maven task

---
+++ src/main/java/net/praqma/vans/task/Maven.java
@@ -0,0 +1,18 @@
+package net.praqma.vans.task;
+
+import java.io.File;
+
+import net.praqma.vans.configuration.ProjectParser;
+
+
+public class Maven extends Shell
+{
+
+	public Maven( File path, ProjectParser conf )
+	{
+		super( path, "" );
+		
+		
+	}
+
+}
