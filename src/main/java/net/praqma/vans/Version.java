86172ff3da3a2f7750620368620bf5ff9a62349b
wolfgang <chw@praqma.net>
2011-02-04 15:05:47 +0100

    Added version

---
+++ src/main/java/net/praqma/vans/Version.java
@@ -0,0 +1,15 @@
+package net.praqma.vans;
+
+/**
+ * @author wolfgang
+ * 
+ */
+public class Version
+{
+	private static final String major    = "0"; // buildnumber.major
+	private static final String minor    = "0"; // buildnumber.minor
+	private static final String patch    = "1"; // buildnumber.patch
+	private static final String sequence = "0"; // buildnumber.sequence
+	
+	public static final  String version  = major + '.' + minor + '.' + patch + '.' + sequence;
+}
